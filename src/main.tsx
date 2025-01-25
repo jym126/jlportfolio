18d757ce4e570a144eade5f42aee8ab42342c89e
jym126 <jym126@hotmail.com>
2025-01-25 21:04:04 +0100

    routes2

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './App.css'
-import App from './App.tsx'
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+    <Navbar />
+      <div className="content">
+        <Routes>
+          <Route path="/jlportfolio/jlportfolio" element={<Home />} />
+          <Route path="/jlportfolio/about" element={<About />} />
+          <Route path="/jlportfolio/projects" element={<Projects />} />
+          <Route path="/jlportfolio/contact" element={<Contact />} />
+        </Routes>
+      </div>
+      <Footer />
     </BrowserRouter>
   </StrictMode>,
 )
