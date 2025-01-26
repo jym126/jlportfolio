446392472f87283f3bc7d1456104323de88d332b
jym126 <jym126@hotmail.com>
2025-01-26 23:47:53 +0100

    links3

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -49,17 +49,32 @@ const Projects: React.FC = () => {
       url: 'https://distecalc.netlify.app/'
     },
     {
-      title: 'chatdroid',
-      description: 'Chat interactivo con escritora y reconocimiento de voz. Desarrollada con Python y Sockets',
-      image: 'https://res.cloudinary.com/dmimages/image/upload/v1737917094/chatdroid_x1ffus.jpg',
-      url: 'https://chatdroid.onrender.com/'
+      title: 'sharedden',
+      description: 'Real estate portal for searching shared apartments. Developed with MERN Mongobd, Express, JavaScript and Nodejs',
+      image: 'https://res.cloudinary.com/dmimages/image/upload/v1737930689/sharedden_a8quf2.jpg',
+      url: '#'
     },
   ];
 
   const projectData2 = [
-    { title: 'Project 1', description: 'Description of Project 1' },
-    { title: 'Project 2', description: 'Description of Project 2' },
-    { title: 'Project 3', description: 'Description of Project 3' },
+    {
+      title: 'gamesReview',
+      description: 'Android Application that allows viewing the latest released games and their features. Developed with Ionic',
+      image: 'https://res.cloudinary.com/dmimages/image/upload/v1737928630/games-review_tsjdmp.jpg',
+      url: 'https://play.google.com/store/apps/details?id=com.developmentmartinez.gamereview'
+    },
+    {
+      title: 'Busca-Gif',
+      description: 'Android Application that allows us find gifs introducing a name. Developed with Ionic',
+      image: 'https://res.cloudinary.com/dmimages/image/upload/v1737931060/busca-gif_h2hr5y.jpg',
+      url: 'https://play.google.com/store/apps/details?id=com.developmentmartinez.giffinder'
+    },
+    {
+      title: 'GuiaTV-pelis y series',
+      description: 'Android Application that allows viewing the latest released movies and series, actor and sipnosis. Developed with Ionic',
+      image: 'https://res.cloudinary.com/dmimages/image/upload/v1737931510/GuiaTV-pelis_y_series_nzkban.jpg',
+      url: 'https://chatdroid.onrender.com/'
+    },
   ];
 
   return (
@@ -86,12 +101,13 @@ const Projects: React.FC = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {projectData2.map((project, index) => (
           <div
-            key={index}
-            className="bg-gray-800 p-6 rounded-lg shadow-lg hover:shadow-xl transition"
-          >
-            <h2 className="text-xl font-semibold mb-2">{project.title}</h2>
-            <p>{project.description}</p>
-          </div>
+          key={index}
+          className="bg-gray-800 p-6 rounded-lg shadow-lg hover:shadow-xl transition"
+        >
+          <h2 className="text-xl font-semibold mb-2">{project.title}</h2>
+          <p>{project.description}</p>
+          <a href={project.url} target="_blank"><img src={project.image} alt="Project 1" className="w-full h-40 mt-4 object-cover" /></a>
+        </div>
         ))}
       </div>
     </div>
